f323c1653b8dc11790bc26cf4773a2359ae2e70c
Tim Song <timsong@uvic.ca>
2011-10-04 16:53:46 -0700

    Added some matrix code.

--- matrix.h
+++ matrix.h
@@ -1,12 +1,33 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-typedef unsigned int uint;
+#include "vector.h"
 
-template <typename T>
+template <typename T, uint M, uint N>
 class Matrix {
 public:
-    Matrix();
+    Matrix() {
+        this->zero();
+    }
+
+    Matrix(const Matrix<T,M,N>& M) {
+        for (uint i = 0; i < M; i++)
+            for (uint j = 0; j < N; j++)
+                this->data[i][j] = M.data[i][j];
+    }
+
+    void zero() {
+        for (uint i = 0; i < M; i++)
+            for (uint j = 0; j < N; j++)
+                this->data[i][j] = 0;
+    }
+
+   /* Matrix<T,M,N>& operator+(const Matrix<T,M,N>& M) const {
+
+    }*/
+
+protected:
+    T data[M][N];
 };
 
 #endif // MATRIX_H
